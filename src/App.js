662f1a6221343fe55fafc3013d04de3efd872fc0
sanjeewa <sanjeewa@techleadintl.com>
2021-05-28 02:47:26 +0530

    changed route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter  as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CityWeather from "./pages/CityWeather";
 import { WeatherProvider } from "./reducer/weatherProvider";
 
+console.log('aaadppp',process.env.PUBLIC_URL)
+
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <WeatherProvider>
         <Route path="/" exact component={Home} />
         <Route path="/cityweather/:name" component={CityWeather} />
