c3b231a732f142081680a1cc18a8a6a0ad29aa1f
Sanjeewa Ranuluge <sranuluge.dev@gmail.com>
2023-10-31 14:40:29 +1300

    feat: updated routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import { HashRouter as Router, Route } from "react-router-dom";
+import {
+  createHashRouter,
+  Route,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import CityWeather from "./pages/CityWeather";
 import { WeatherProvider } from "./reducer/weatherProvider";
 
+const router = createHashRouter(
+  createRoutesFromElements(
+    <>
+      <Route path={"/"} index element={<Home />} />
+      <Route path={"/cityweather/:name"} element={<CityWeather />} />
+    </>
+  )
+);
+
 function App() {
   return (
-    <Router>
-      <WeatherProvider>
-        <Route path="/" exact component={Home} />
-        <Route path="/cityweather/:name" component={CityWeather} />
-      </WeatherProvider>
-    </Router>
+    <WeatherProvider>
+      <RouterProvider router={router} />
+    </WeatherProvider>
   );
 }
 
